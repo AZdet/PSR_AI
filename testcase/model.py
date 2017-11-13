677b26312e0e94164a1b363b0851e5549c81982a
baislsl <baislsl666@gmail.com>
2017-11-13 16:09:55 +0800

    fix bugs when using keras

--- testcase/model.py
+++ testcase/model.py
@@ -1,40 +1,41 @@
-
 # coding: utf-8
 
-# In[2]:
+# In[24]:
+
 
 import keras
 from keras.models import Sequential
 from keras.layers import *
 
+# In[25]:
 
-# In[3]:
 
 import numpy as np
 
+# In[26]:
 
-# In[120]:
 
 time_step = 10
 BATCH_SIZE = 32
 EPOCH = 10
 
+# In[27]:
 
-# In[116]:
 
 model = Sequential()
-model.add(Embedding(9 , 9, input_length= 1))
-model.add(GRU(5, input_shape = (None, 1)))
+model.add(Embedding(9, 9, input_length=1))
+model.add(GRU(5, input_shape=(None, 1)))
 model.add(Dense(3))
 model.add(Activation("softmax"))
 
+# In[28]:
 
-# In[117]:
 
 model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
 
 
-# In[26]:
+# In[29]:
+
 
 class Dictionary(object):
     def __init__(self):
@@ -49,7 +50,8 @@ class Dictionary(object):
 
     def __len__(self):
         return len(self.idx2word)
-    
+
+
 def one_hots(numerical_list, vocab_size):
     result = np.zeros((len(numerical_list), vocab_size))
     for i, idx in enumerate(numerical_list):
@@ -57,39 +59,45 @@ def one_hots(numerical_list, vocab_size):
     return result
 
 
-# In[27]:
+# In[30]:
+
 
 data_dict = Dictionary()
+
+
 def tokenize(dic, path):
-        """Tokenizes a text file."""
-        # Add words to the dictionary
-        with open(path, 'r') as f:    # in my case, only one word each line
-            tokens = 0
-            for line in f:
-                #words = line.split() + ['<eos>']
-                tokens += 1#len(words)
-                #for word in words:
-                    #self.dictionary.add_word(word)
-                dic.add_word(line)
+    """Tokenizes a text file."""
+    # Add words to the dictionary
+    with open(path, 'r') as f:  # in my case, only one word each line
+        tokens = 0
+        for line in f:
+            # words = line.split() + ['<eos>']
+            tokens += 1  # len(words)
+            # for word in words:
+            # self.dictionary.add_word(word)
+            dic.add_word(line)
 
-        # Tokenize file content
-        with open(path, 'r') as f:
-            ids = np.zeros((tokens,), dtype='int32')
-            token = 0
-            for line in f:
-                #words = line.split() + ['<eos>']
-                #for word in words:
-                    #ids[token] = self.dictionary.word2idx[word]
-                    #token += 1
-                ids[token] = dic.word2idx[line]
-                token += 1
+    # Tokenize file content
+    with open(path, 'r') as f:
+        ids = np.zeros((tokens,), dtype='int32')
+        token = 0
+        for line in f:
+            # words = line.split() + ['<eos>']
+            # for word in words:
+            # ids[token] = self.dictionary.word2idx[word]
+            # token += 1
+            ids[token] = dic.word2idx[line]
+            token += 1
 
-        return ids
+    return ids
 
 
-# In[96]:
+# In[31]:
+
 
 """make sure that data_dict is initialized"""
+
+
 def tokenize_list(lst):
     tokens = lst.len
     ids = np.zeros((tokens,), dtype='int32')
@@ -100,106 +108,107 @@ def tokenize_list(lst):
     return ids
 
 
-# In[ ]:
+# In[32]:
 
-
-
-
-# In[28]:
 
 data1 = tokenize(data_dict, 'test6.txt')
 
+# In[33]:
 
-# In[154]:
 
 data_dict.word2idx.items()
 
+# In[34]:
 
-# In[155]:
 
 data_dict.idx2word
 
-
-# In[37]:
-
-data = one_hots(data1[0:30] ,9)
+# In[35]:
 
 
-# In[81]:
+data = one_hots(data1[0:30], 9)
+
+# In[36]:
+
 
 data2 = data1[:5000]
 
+# In[37]:
 
-# In[92]:
 
 data1
 
+# In[38]:
 
-# In[32]:
 
 label_dict = Dictionary()
 label1 = tokenize(label_dict, 'label2.txt')
 
+# In[39]:
 
-# In[153]:
 
 label_dict.idx2word
 
+# In[40]:
 
-# In[86]:
 
 label = one_hots(label1, 3)
 
+# In[41]:
 
-# In[137]:
 
 label
 
+# In[42]:
 
-# In[161]:
 
 test_data = tokenize(data_dict, 'test7.txt')
 label2 = tokenize(label_dict, 'label3.txt')
 test_label = one_hots(label2, 3)
 
-
-# In[ ]:
-
+# In[47]:
 
 
+label
 
-# In[158]:
-
-model.fit(data[:-1], label, batch_size = BATCH_SIZE, epochs = EPOCH)
+# In[46]:
 
 
-# In[122]:
+data
+
+# In[53]:
+
+
+model.fit(data1[:-1], label, batch_size=BATCH_SIZE, epochs=EPOCH)
+
+# In[54]:
+
 
 test_data = data1[5000:-1]
 test_label = label[5000:]
 
-
-# In[162]:
-
-model.evaluate(test_data[:-1], test_label)
+# In[56]:
 
 
-# In[159]:
+model.evaluate(test_data, test_label)
+
+# In[57]:
+
 
 model.predict(np.array([0, 1, 2, 3, 4, 5, 6, 7, 8]))
 
+# In[58]:
 
-# In[127]:
 
 np.argmax(model.predict(np.array([1])))
 
+# In[59]:
 
-# In[129]:
 
 import random as rd
 
-
 # In[ ]:
+
 
 lookTab = {"r": 0, "s": 1, "p": 2}
 [r, s, p] = ["r", "s", "p"]
@@ -208,8 +217,9 @@ alpha = 1.1
 upbound = 5000
 [p11, p12, p13] = [0.001, 0.004, 0.995]  # w-, w+, w0
 [p21, p22, p23] = [0.063, 0.791, 0.146]  # t-, t+, t0
-[p31, p32, p33] = [0.989, 0.001, 0.01] # l-, l+, l0
+[p31, p32, p33] = [0.989, 0.001, 0.01]  # l-, l+, l0
 (win, tie, lose) = (1, 0, -1)
+
 
 def generateAction(p1, p2, p3):
     n = rd.random()
@@ -224,12 +234,10 @@ def generateAction(p1, p2, p3):
 def getRes(action1, action2):
     if action1 == action2:
         return (tie, tie)
-    elif (action1 == r and action2 == s)or         (action1 == s and action2 == p)or         (action1 == p and action2 == r):
+    elif (action1 == r and action2 == s) or (action1 == s and action2 == p) or (action1 == p and action2 == r):
         return win, lose
     else:
         return lose, win
-
-
 
 
 class Player:
@@ -239,10 +247,10 @@ class Player:
 
     def getAction(self):
         idx = lookTab[self.action]
-        pr = [1/3, 1/3, 1/3]
+        pr = [1 / 3, 1 / 3, 1 / 3]
         if self.state == win:
             pr[idx] = p13
-            pr[(idx+1)%3] = p11
+            pr[(idx + 1) % 3] = p11
             idx -= 1
             if idx < 0:
                 idx = 2
@@ -254,7 +262,7 @@ class Player:
             if idx < 0:
                 idx = 2
             pr[idx] = p22
-        else: #self.state == lose
+        else:  # self.state == lose
             pr[idx] = p33
             pr[(idx + 1) % 3] = p31
             idx -= 1
@@ -263,91 +271,92 @@ class Player:
             pr[idx] = p32
         return generateAction(pr[0], pr[1], pr[2])
 
-
     def setAction(self, new_action):
         self.action = new_action
+
 
 class AI(Player):
     def __init__(self):
         Player.__init__(self)
         self.time = 0
         self.lst = []
-        
+
     def receiveHistory(self, history):
         self.lst.append(history)
-        
+
     def getAction(self):
         if self.time < 1:
             print("first time")
             self.time += 1
-            #self.lst.append(opposite_action + str(-self.state) + '\n')
-            return generateAction(1/3, 1/3, 1/3)
+            # self.lst.append(opposite_action + str(-self.state) + '\n')
+            return generateAction(1 / 3, 1 / 3, 1 / 3)
         else:
-            #last_action = np.array([data_dict.word2idx[self.lst[-1]]])
-            #last_state = opposite_action + str(-self.state) + '\n'
+            # last_action = np.array([data_dict.word2idx[self.lst[-1]]])
+            # last_state = opposite_action + str(-self.state) + '\n'
             last_data = np.array([data_dict.word2idx[self.lst[-1]]])
             idx = np.argmax(model.predict(last_data))
             res = label_dict.idx2word[idx]
-            #print(words)
-            #symbols_in_keys = [dictionary[str(words[i])] for i in range(len(words))]
-            #keys = np.reshape(np.array(symbols_in_keys), [-1, 3, 1])
-            #onehot_pred = sess.run(pred, feed_dict={x: keys})
-            #onehot_pred_index = int(tf.argmax(onehot_pred, 1).eval(session=sess))
-            #symbols_in_keys = symbols_in_keys[1:]
-            #symbols_in_keys.append(onehot_pred_index)
-            #self.lst = self.lst[1:]
-            #self.lst.append(opposite_action + str(-self.state) + '\n')
-            #res =  reverse_dictionary[onehot_pred_index][0]
-            
-            #assert(res in actions)
-            if res == 'r\n':   # shift the predicted action to win the game 
+            # print(words)
+            # symbols_in_keys = [dictionary[str(words[i])] for i in range(len(words))]
+            # keys = np.reshape(np.array(symbols_in_keys), [-1, 3, 1])
+            # onehot_pred = sess.run(pred, feed_dict={x: keys})
+            # onehot_pred_index = int(tf.argmax(onehot_pred, 1).eval(session=sess))
+            # symbols_in_keys = symbols_in_keys[1:]
+            # symbols_in_keys.append(onehot_pred_index)
+            # self.lst = self.lst[1:]
+            # self.lst.append(opposite_action + str(-self.state) + '\n')
+            # res =  reverse_dictionary[onehot_pred_index][0]
+
+            # assert(res in actions)
+            if res == 'r\n':  # shift the predicted action to win the game
                 return 'p'
             elif res == 's\n':
                 return 'r'
-            else: # res == 'p'
+            else:  # res == 'p'
                 return 's'
             return res
-        
+
+
 p1 = Player()
 p2 = AI()
-action1 = generateAction(1/3, 1/3, 1/3)
-action2 = generateAction(1/3, 1/3, 1/3)
+action1 = generateAction(1 / 3, 1 / 3, 1 / 3)
+action2 = generateAction(1 / 3, 1 / 3, 1 / 3)
 p1.setAction(action1)
 p2.setAction(action2)
 (state1, state2) = getRes(action1, action2)
 p1.state = state1
 p2.state = state2
 p2.receiveHistory(action1 + str(state1) + '\n')
-f = open('./res.txt', 'w+')#'C:\Users\lenovo\Documents\SRTP\PSR_AI\testcase\test.txt', 'w')
+f = open('./res.txt', 'w+')  # 'C:\Users\lenovo\Documents\SRTP\PSR_AI\testcase\test.txt', 'w')
 action1s = []
 action2s = []
-#action1 = action2 = None
+# action1 = action2 = None
 stat = {-1: 0, 0: 0, 1: 0}
 for i in range(upbound):
     action1 = p1.getAction()
     action2 = p2.getAction()
     p1.setAction(action1)
     p2.setAction(action2)
-    #print("action(p1,p2): ",action1," ",action2, "\n")
+    # print("action(p1,p2): ",action1," ",action2, "\n")
     action1s.append(action1)
     action2s.append(action2)
-    #f.write(action1 + ' ' + action2 + ' ')
+    # f.write(action1 + ' ' + action2 + ' ')
     (state1, state2) = getRes(action1, action2)
     stat[state1] += 1
     p1.state = state1
     p2.state = state2
     p2.receiveHistory(action1 + str(state1) + '\n')
-#count = 0
-#for ac1, ac2 in zip(action1s, action2s):
+# count = 0
+# for ac1, ac2 in zip(action1s, action2s):
 #    f.write(ac1 + ' ' + ac2 + ' ' + str(getRes(ac1, ac2)[0]) + '\n')#ac2 + '\n')#+ ' ' + 
 #    count += getRes(ac1, ac2)[0]
-#f.close()
-#print(count)
+# f.close()
+# print(count)
 print(stat.items())
 print('ok')
 
-
 # In[ ]:
+
 
 lookTab = {"r": 0, "s": 1, "p": 2}
 [r, s, p] = ["r", "s", "p"]
@@ -356,8 +365,9 @@ alpha = 1.1
 upbound = 5000
 [p11, p12, p13] = [0.001, 0.004, 0.995]  # w-, w+, w0
 [p21, p22, p23] = [0.063, 0.791, 0.146]  # t-, t+, t0
-[p31, p32, p33] = [0.989, 0.001, 0.01] # l-, l+, l0
+[p31, p32, p33] = [0.989, 0.001, 0.01]  # l-, l+, l0
 (win, tie, lose) = (1, 0, -1)
+
 
 def generateAction(p1, p2, p3):
     n = rd.random()
@@ -372,12 +382,10 @@ def generateAction(p1, p2, p3):
 def getRes(action1, action2):
     if action1 == action2:
         return (tie, tie)
-    elif (action1 == r and action2 == s)or         (action1 == s and action2 == p)or         (action1 == p and action2 == r):
+    elif (action1 == r and action2 == s) or (action1 == s and action2 == p) or (action1 == p and action2 == r):
         return win, lose
     else:
         return lose, win
-
-
 
 
 class Player:
@@ -387,76 +395,76 @@ class Player:
 
     def getAction(self):
         act = input(">input: ")
-        assert(act in actions)
+        assert (act in actions)
         return act
-
 
     def setAction(self, new_action):
         self.action = new_action
+
 
 class AI(Player):
     def __init__(self):
         Player.__init__(self)
         self.time = 0
         self.lst = []
-        
+
     def receiveHistory(self, history):
         self.lst.append(history)
-        
+
     def getAction(self):
         if self.time < 1:
             print("first time")
             self.time += 1
-            return generateAction(1/3, 1/3, 1/3)
+            return generateAction(1 / 3, 1 / 3, 1 / 3)
         else:
             last_data = np.array([data_dict.word2idx[self.lst[-1]]])
             idx = np.argmax(model.predict(last_data))
             res = label_dict.idx2word[idx]
-        
-            if res == 'r\n':   # shift the predicted action to win the game 
+
+            if res == 'r\n':  # shift the predicted action to win the game
                 return 'p'
             elif res == 's\n':
                 return 'r'
-            else: # res == 'p'
+            else:  # res == 'p'
                 return 's'
             return res
-        
+
+
 p1 = Player()
 p2 = AI()
-action1 = generateAction(1/3, 1/3, 1/3)
-action2 = generateAction(1/3, 1/3, 1/3)
+action1 = generateAction(1 / 3, 1 / 3, 1 / 3)
+action2 = generateAction(1 / 3, 1 / 3, 1 / 3)
 p1.setAction(action1)
 p2.setAction(action2)
 (state1, state2) = getRes(action1, action2)
 p1.state = state1
 p2.state = state2
 p2.receiveHistory(action1 + str(state1) + '\n')
-#f = open('./res.txt', 'w+')#'C:\Users\lenovo\Documents\SRTP\PSR_AI\testcase\test.txt', 'w')
+# f = open('./res.txt', 'w+')#'C:\Users\lenovo\Documents\SRTP\PSR_AI\testcase\test.txt', 'w')
 action1s = []
 action2s = []
-#action1 = action2 = None
+# action1 = action2 = None
 stat = {-1: 0, 0: 0, 1: 0}
 for i in range(upbound):
     action1 = p1.getAction()
     action2 = p2.getAction()
     p1.setAction(action1)
     p2.setAction(action2)
-    #print("action(p1,p2): ",action1," ",action2, "\n")
+    # print("action(p1,p2): ",action1," ",action2, "\n")
     action1s.append(action1)
     action2s.append(action2)
-    #f.write(action1 + ' ' + action2 + ' ')
+    # f.write(action1 + ' ' + action2 + ' ')
     (state1, state2) = getRes(action1, action2)
     stat[state1] += 1
     p1.state = state1
     p2.state = state2
     p2.receiveHistory(action1 + str(state1) + '\n')
     print(stat.items())
-#count = 0
-#for ac1, ac2 in zip(action1s, action2s):
+# count = 0
+# for ac1, ac2 in zip(action1s, action2s):
 #    f.write(ac1 + ' ' + ac2 + ' ' + str(getRes(ac1, ac2)[0]) + '\n')#ac2 + '\n')#+ ' ' + 
 #    count += getRes(ac1, ac2)[0]
-#f.close()
-print(count)
+# f.close()
+# print(count)
 print(stat.items())
 print('ok')
-
