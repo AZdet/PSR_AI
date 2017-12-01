b7fea3f75c0a43a8152a0d2111a9e64d41a3b1f2
baislsl <baislsl666@gmail.com>
2017-12-01 22:50:52 +0800

    add softmax when a = 2

--- module/keras.py
+++ module/keras.py
@@ -26,10 +26,14 @@ class Keras:
         return self.train(data, label)
 
     def train(self, data, label):
-        return self.model.fit(data[:-1], label, batch_size=self.batch_size, epochs=self.epoch)
+        result = self.model.fit(data[:-1], label, batch_size=self.batch_size, epochs=self.epoch)
+        print(self.model.predict(np.array([0, 1, 2, 3, 4, 5, 6, 7, 8])))
+        return result
 
     def predict(self, x, verbose=0):
-        return self.model.predict(x, batch_size=self.batch_size, verbose=verbose)
+        result = self.model.predict(x, batch_size=self.batch_size, verbose=verbose)
+        print(result)
+        return result
 
     def evaluate_path(self, data_path, label_path):
         data = tokenize(self.data_dict, data_path)
